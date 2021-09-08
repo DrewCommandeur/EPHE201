44569991cbb52927f4017b8c0def743a6ffd9407
dcommandeur <65050352+DrewCommandeur@users.noreply.github.com>
2021-09-08 13:20:05 -0700

    Update EPHE201_Lab3_LinKin.py

--- LinKin/EPHE201_Lab3_LinKin.py
+++ LinKin/EPHE201_Lab3_LinKin.py
@@ -11,7 +11,7 @@ import streamlit as st
 from scipy import integrate
 import matplotlib.pyplot as plt 
 
-sampleData = "https://raw.githubusercontent.com/DrewCommandeur/EPHE201/main/LinKin/25m_SprintData.csv"
+sampleData = "https://raw.githubusercontent.com/DrewCommandeur/EPHE201/866d4bef6d883fdd719b53851259ef6accb28d48/LinKin/25m_SprintData.csv"
 
 st.title('Lab 3: Linear Kinematics- Sprint Analysis') #Title for Streamlit
 st.write("*In this lab, you will learn how to calculate kinematic values from raw IMU sprinting data.*")
